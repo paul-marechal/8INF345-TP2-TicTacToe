e801bd33e37ec9c65b04fd7d9412bc86f83898eb
WKnight02 <wknight02@gmail.com>
2017-03-13 12:10:36 -0400

    Local network

--- WebApp.py
+++ WebApp.py
@@ -1,6 +1,9 @@
 # -*- coding:utf8 -*-
 from WebTicTacToe import Server
 import WebTicTacToe.Game
+import socket
 
-Server.initialize(baseDir="./ClientSide")
+ip = socket.gethostbyname(socket.gethostname())
+
+Server.initialize(address=ip, baseDir="./ClientSide")
 Server.run()
